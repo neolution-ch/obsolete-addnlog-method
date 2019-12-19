a98bebdf44bc0c828792fbfb72eecf06b16b8450
Sandro Ciervo <sc@neolution.ch>
2019-12-19 16:27:58 +0100

    Add logging config

--- ObsoleteAddNLog/Global.asax.cs
+++ ObsoleteAddNLog/Global.asax.cs
@@ -7,12 +7,45 @@ using System.Web.Routing;
 
 namespace ObsoleteAddNLog
 {
+    using Autofac;
+    using Autofac.Integration.Mvc;
+    using Microsoft.Extensions.Logging;
+    using NLog.Extensions.Logging;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(CreateAutofacContainer()));
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private static IContainer CreateAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Add support for Microsoft Core Logging
+            builder.RegisterType<LoggerFactory>().As<ILoggerFactory>().SingleInstance();
+            builder.RegisterGeneric(typeof(Logger<>)).As(typeof(ILogger<>)).SingleInstance();
+
+            // Build the container
+            var container = builder.Build();
+
+            ConfigureContainer(container);
+
+            return container;
+        }
+
+        private static void ConfigureContainer(IComponentContext context)
+        {
+            // Add NLog to Microsoft Core Logging
+            var loggerFactory = context.Resolve<ILoggerFactory>();
+            loggerFactory.AddNLog();
         }
     }
 }
