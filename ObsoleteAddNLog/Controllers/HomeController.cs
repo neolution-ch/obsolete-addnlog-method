95faee2f05cc6145d2f274736c3fa664489574d8
Sandro Ciervo <sc@neolution.ch>
2019-12-19 16:34:42 +0100

    Added home controller with index action and logging

---
+++ ObsoleteAddNLog/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿namespace ObsoleteAddNLog.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            this.logger.LogInformation("The home page was requested.");
+            return Content("Foobar");
+        }
+    }
+}
